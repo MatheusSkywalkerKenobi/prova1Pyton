cc009c9d1deaf81a32f8d6a5a04796996dec99ef
MatheusSkywalkerKenobi <zeusmachado007@gmail.com>
2026-03-31 12:53:49 -0300

    Add files via upload

---
+++ notas.py
@@ -0,0 +1,6 @@
+a = float(input("Digite a nota do primeiro bimestre"))
+b = float(input("Digite a nota do segundo bimestre"))
+c = float(input("Digite a nota do terceito bimestre"))
+d = (a+b+c)/3
+print(d)
+#--Matheus Soares Machado--
